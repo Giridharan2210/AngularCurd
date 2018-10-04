c8dcfe2f8375cdb669f5c12330036b0c8979f472
Giridharan2210 <giri.2210@gmail.com>
2018-10-04 19:03:40 +0530

    observable changes

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -27,12 +27,6 @@ export class EmployeeComponent implements OnInit {
       Office : new FormControl('',Validators.required),
       Position : new FormControl('',Validators.required)
     });
-
-    // this.profileForm = new FormGroup({
-    //   profileCode : new FormControl(2),
-    //   firstName : new FormControl('',Validators.required),
-    //   lastName : new FormControl('',Validators.required)
-    // })
   }
 
   resetForm(form?: NgForm) {
@@ -52,21 +46,21 @@ export class EmployeeComponent implements OnInit {
     console.log(this.profileForm.value);
   }
 
-  onSubmit(form: NgForm) {
-    if (form.value.EmployeeID == null) {
-      form.value.EmployeeID = 0;
-      this.employeeService.postEmployee(form.value)
+  onSubmit() {
+    if (this.employeeForm.value.EmployeeID == null) {
+      this.employeeForm.value.EmployeeID = 0;
+      this.employeeService.postEmployee(this.employeeForm.value)
         .subscribe(data => {
           // callback functions on post data 
-          this.resetForm(form);
+          this.resetForm();
           this.employeeService.getEmployeeList();
           this.toastr.success('New Record Added Succcessfully', 'Employee Register');
         })
     }
     else {
-      this.employeeService.putEmployee(form.value.EmployeeID, form.value)
+      this.employeeService.putEmployee(this.employeeForm.value.EmployeeID, this.employeeForm.value)
       .subscribe(data => {
-        this.resetForm(form);
+        this.resetForm();
         this.employeeService.getEmployeeList();
         this.toastr.info('Record Updated Successfully!', 'Employee Register');
       });
