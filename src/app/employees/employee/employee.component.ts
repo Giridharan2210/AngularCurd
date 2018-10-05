0c75e44ef6257f5f0141a4b6b5d7bc1bedb8cc11
Giridharan2210 <giri.2210@gmail.com>
2018-10-05 15:25:48 +0530

    reactive form validation on sumit

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms'
 
 import { EmployeeService } from '../../shared/employee.service'
@@ -20,13 +20,13 @@ export class EmployeeComponent implements OnInit {
     this.resetForm();
 
     this.employeeForm = new FormGroup({
-      EmployeeID : new FormControl(),
-      FirstName : new FormControl('',Validators.required),
-      LastName : new FormControl('',Validators.required),
-      EmpCode : new FormControl('',Validators.required),
-      Office : new FormControl('',Validators.required),
-      Position : new FormControl('',Validators.required)
-    });
+          EmployeeID : new FormControl(),
+          FirstName : new FormControl('',Validators.required),
+          LastName : new FormControl('',Validators.required),
+          EmpCode : new FormControl(),
+          Office : new FormControl(),
+          Position : new FormControl()
+        });
   }
 
   resetForm(form?: NgForm) {
@@ -46,24 +46,42 @@ export class EmployeeComponent implements OnInit {
     console.log(this.profileForm.value);
   }
 
+  isFieldValid(field: string) {
+    return !this.employeeForm.get(field).valid && this.employeeForm.get(field).dirty;
+  }
+
   onSubmit() {
-    if (this.employeeForm.value.EmployeeID == null) {
-      this.employeeForm.value.EmployeeID = 0;
-      this.employeeService.postEmployee(this.employeeForm.value)
+    if(this.employeeForm.valid){
+      if (this.employeeForm.value.EmployeeID == null) {
+        this.employeeForm.value.EmployeeID = 0;
+        this.employeeService.postEmployee(this.employeeForm.value)
+          .subscribe(data => {
+            // callback functions on post data 
+            this.resetForm();
+            this.employeeService.getEmployeeList();
+            this.toastr.success('New Record Added Succcessfully', 'Employee Register');
+          })
+      }
+      else {
+        this.employeeService.putEmployee(this.employeeForm.value.EmployeeID, this.employeeForm.value)
         .subscribe(data => {
-          // callback functions on post data 
           this.resetForm();
           this.employeeService.getEmployeeList();
-          this.toastr.success('New Record Added Succcessfully', 'Employee Register');
-        })
+          this.toastr.info('Record Updated Successfully!', 'Employee Register');
+        });
+      }
+    }else{
+        this.validateAllFormFields(this.employeeForm); 
     }
-    else {
-      this.employeeService.putEmployee(this.employeeForm.value.EmployeeID, this.employeeForm.value)
-      .subscribe(data => {
-        this.resetForm();
-        this.employeeService.getEmployeeList();
-        this.toastr.info('Record Updated Successfully!', 'Employee Register');
+  }
+  validateAllFormFields(formGroup: FormGroup) {        
+      Object.keys(formGroup.controls).forEach(field => {  
+        const control = formGroup.get(field);             
+        if (control instanceof FormControl) {            
+          control.markAsDirty({ onlySelf: true });
+        } else if (control instanceof FormGroup) {        
+          this.validateAllFormFields(control);            
+        }
       });
-    }
   }
 }
